bda409c4e7368c388ece50c386475b1a2824845e
skadbstj12 <163810643+skadbstj12@users.noreply.github.com>
2024-05-03 01:22:46 +0900

    Update lezincomics.py

--- lezincomics.py
+++ lezincomics.py
@@ -10,31 +10,31 @@ import time
 import json
 from datetime import datetime
 
-# Chrome 옵션 설정
-options = Options()
-options.add_argument('--headless')  # headless 모드 활성화
-options.add_argument('--no-sandbox')  # 샌드박스 모드 비활성화
-options.add_argument('--disable-dev-shm-usage')  # /dev/shm 사용 제한
 
-# Chrome 드라이버 서비스 설정
+options = Options()
+options.add_argument('--headless')  
+options.add_argument('--no-sandbox') 
+options.add_argument('--disable-dev-shm-usage') 
+
+
 service = ChromeService(ChromeDriverManager().install())
 
-# 드라이버 초기화
+
 driver = webdriver.Chrome(service=service, options=options)
 
-# 웹 드라이브를 설정하여 페이지에 접근
+
 driver.get("https://bufftoon.plaync.com/tag/ranking?currentType=webtoon")
 
-# 페이지가 완전히 로드될 때까지 대기
+
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "width-box"))
 )
 
-# 천천히 스크롤 다운
-scroll_pause_time = 1  # 1초 대기
-pixels_to_scroll = 200  # 한 번에 스크롤할 픽셀 수
-max_time_limit = 40  # 전체 작업 시간 제한 (40초)
-start_time = time.time()  # 작업 시작 시간
+
+scroll_pause_time = 1  
+pixels_to_scroll = 200  
+max_time_limit = 40  
+start_time = time.time()  
 
 while (time.time() - start_time) < max_time_limit:
     old_height = driver.execute_script("return document.body.scrollHeight")
@@ -42,16 +42,15 @@ while (time.time() - start_time) < max_time_limit:
     time.sleep(scroll_pause_time)
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == old_height:
-        break  # 페이지 끝에 도달
+        break  
 
-# 현재 날짜 가져오기
+
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"bufftoon_{current_date}.json"
 
 html_source_updated = driver.page_source
 soup = BeautifulSoup(html_source_updated, 'html.parser')
 
-# 데이터 추출
 webtoon_data = []
 items = soup.select(".split-item")
 for item in items:
@@ -66,9 +65,9 @@ for item in items:
         "imageURL": image_url
     })
 
-# 데이터를 JSON 파일로 저장
+
 with open(filename, 'w', encoding='utf-8') as f:
     json.dump(webtoon_data, f, ensure_ascii=False, indent=4)
 
-# 브라우저 종료
+
 driver.quit()
