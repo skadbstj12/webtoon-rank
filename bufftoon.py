5ed8a12b890b4dca6cb618215f0c2c970f577e1c
skadbstj12 <163810643+skadbstj12@users.noreply.github.com>
2024-05-07 16:27:19 +0900

    Update bufftoon.py

--- bufftoon.py
+++ bufftoon.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import json
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"bufftoon_{current_date}.json"
 # 웹드라이브 설치
